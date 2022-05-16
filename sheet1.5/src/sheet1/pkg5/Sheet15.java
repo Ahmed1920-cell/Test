224937ee15e5058141da6bb16342e5a8ac655e68
Ahmed1920-cell <70626826+Ahmed1920-cell@users.noreply.github.com>
2022-05-16 15:53:19 +0200

    add project file

---
+++ sheet1.5/src/sheet1/pkg5/Sheet15.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sheet1.pkg5;
+
+/**
+ *
+ * @author ahmed
+ */
+public class Sheet15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(((7.5*6.5)-(4.5*3))/(47.5-5.5));
+    }
+    
+}
