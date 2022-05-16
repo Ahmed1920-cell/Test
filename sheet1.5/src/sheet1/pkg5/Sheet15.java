11410e38ec0a2e4445d5ff70cde58e97ca903fd0
Ahmed1920-cell <70626826+Ahmed1920-cell@users.noreply.github.com>
2022-05-16 15:56:58 +0200

    Revert "change code"
    This reverts commit 0c363330c5d064d5ab2f334c30c8427acca915b2.

--- sheet1.5/src/sheet1/pkg5/Sheet15.java
+++ sheet1.5/src/sheet1/pkg5/Sheet15.java
@@ -16,7 +16,7 @@ public class Sheet15 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(((7.5*6.5)-(4.5*3))/(47.5-5.5)+5);
+        System.out.println(((7.5*6.5)-(4.5*3))/(47.5-5.5));
     }
     
 }
